f9fa66210f72d88264569d7c8d53fb1605b068ab
maxmeng(蒙卓) <maxmeng@tencent.com>
2017-01-09 15:09:44 +0800

    update submod

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+
+	"github.com/fsnotify/fsnotify"
+)
 
 func main() {
 	fmt.Println("vim-go")
+	w, _ := fsnotify.NewWatcher()
+	w.Add(".")
+	time.Sleep(time.Second)
 }
